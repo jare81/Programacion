b835193f2f03f89ea8b7a248c60218398ddd9827
Dell <Dell@DESKTOP-PCISOGC>
2024-08-22 15:39:52 -0600

    BiArray

---
+++ src/Dubon_Jareny_BiArray.java
@@ -0,0 +1,68 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Dubon_Jareny_BiArray {
+    public static void main(String[] args) {
+      
+       Scanner entrada = new Scanner(System.in);
+       
+       Random  random = new Random();
+        System.out.println("Ingrese la cantidad de filas y columnas del arreglo");
+        int cantidad;
+        while (true) {
+                 try {
+                   cantidad = entrada.nextInt();            
+                    while(cantidad<0){
+                        System.out.print("Ingrese numero positivo: ");
+                        cantidad = entrada.nextInt(); }
+                     break;
+                     } catch (InputMismatchException e) {
+                         System.out.print("Error de Formato: Ingrese un numero entero: ");
+                         entrada.next(); }
+                }        
+        
+        
+       
+        System.out.println("");
+        
+        int numeros[][] = new int [cantidad][cantidad];
+        
+        int aumenta=0;
+        int disminuye=(cantidad-1);
+        int sumaPrincipal=0;
+        int sumaSecundaria=0;
+        
+        for (int filas = 0; filas <numeros.length ; filas++) {
+            
+            
+            for (int columnas = 0; columnas < numeros.length; columnas++) {
+                int numeroRan = random.nextInt(100-10) + 10;
+                
+                 numeros[filas][columnas]=numeroRan;   
+                 System.out.print(numeros[filas][columnas] + " ");
+                 
+                 
+                }
+            sumaPrincipal = sumaPrincipal + numeros[filas][aumenta] ;
+            sumaSecundaria= sumaSecundaria+ numeros[filas][(disminuye)];
+            aumenta++;
+            disminuye--;
+            System.out.println("");
+            
+            
+            }
+        
+          System.out.println("\nSuma de la Diagonal Principal " + sumaPrincipal);  
+          System.out.println("Suma de la Diagonal Secundaria: " + sumaSecundaria);
+            
+        }
+        
+        
+        
+        
+        
+        
+    
+    
+}
